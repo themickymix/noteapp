cf0e8640f2ac52a389275bc2fe7b89a1b11ebd6f
themickymix <micz.yalon@gmail.com>
2025-02-02 12:03:19 +0800

    roolback

--- app/test.tsx
+++ app/test.tsx
@@ -1,8 +1,11 @@
-import React from "react";
-import { useRouter } from "next/router";
+"use client";
+
+import React, { useEffect, useState } from "react";
 import Add from "./add";
-import NavBar from "./component/nav-bar";
+import { useRouter } from "next/navigation";
 import Delete from "./delete";
+import NavBar from "./component/nav-bar";
+import axios from "axios";
 
 type Note = {
   _id: string;
@@ -10,20 +13,48 @@ type Note = {
   content: string;
 };
 
-type Props = {
-  notes: Note[];
-  error?: string;
-};
-
-const Test = ({ notes, error }: Props) => {
+function Test() {
   const router = useRouter();
+  const [notes, setNotes] = useState<Note[]>([]);
+  const [error, setError] = useState<string | null>(null);
+
+  const fetchData = async () => {
+    const token = localStorage.getItem("token");
+    try {
+      const response = await axios.get(
+        `${process.env.NEXT_PUBLIC_BACKEND_URL}/api/v1/`,
+        {
+          withCredentials: true,
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      );
+      const data = response.data;
+
+      const validNotes = data.filter(
+        (note: Note) => note.title?.trim() || note.content?.trim()
+      );
+
+      setNotes(validNotes);
+      setError(null);
+    } catch (error) {
+      console.error("Error fetching data:", error);
+      setError("Failed to fetch notes. Please try again later.");
+    }
+  };
+
+  useEffect(() => {
+    fetchData(); // Fetch notes initially
+  }, [notes]);
 
   return (
     <div>
       <NavBar />
       <Add />
       {error && <p className="text-red-500 text-center mt-4">{error}</p>}
-      <div className="grid grid-cols-2 md:grid-cols-4 lg:grid-cols-5 gap-4 mt-8">
+      <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-4 mt-8">
         {notes.map((note: Note) => (
           <div key={note._id}>
             <div className="relative">
@@ -47,6 +78,6 @@ const Test = ({ notes, error }: Props) => {
       </div>
     </div>
   );
-};
+}
 
 export default Test;
